9e019bc21774bd71c51c5501191fde3559a63240
Weronika Tomaszewska <vikariusu@gmail.com>
2017-06-07 15:14:01 +0200

    fixed seed, added forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: [:index, :create, :show, :new, :destroy] do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
+
+
+
+
+
+  # # a visitor can display restaurants
+  # get "restaurants", to: "restaurants#index"
+  # # a visitor can create a new restaurant
+  # post "restaurants", to: "restaurants#create"
+  # # get a form to create a new restaurant
+  # get "restaurants/new", to: "restaurants#new"
+  # # display a restaurant with given id
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Restaurant.create(name: "Bicicleta", address: "Gracia", phone_number: "43223142", category: "italian")
-Restaurant.create(name: "Chineses omnom", address: "Somewhere", phone_number: "12313142", category: "chinese")
+Restaurant.create(name: "Memorias de China", address: "Carrer de Lincoln", phone_number: "12313142", category: "chinese")
+Restaurant.create(name: "Wagokoro", address: "Carrer de Regas", phone_number: "62482599", category: "japanese")
+Restaurant.create(name: "P'tit Bistro", address: "Carrer de la Diputació", phone_number: "98032124", category: "french")
+Restaurant.create(name: "Betawi", address: "Carrer de Montsio", phone_number: "45230231", category: "chinese")
+
